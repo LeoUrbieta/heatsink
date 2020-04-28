711d7162a9123e70da86690b9999114df84bda85
Leonardo Moreno Urbieta <leourbieta@MacBook-Pro-de-Leonardo.local>
2020-04-28 16:48:36 -0500

    Mas adaptaciones para exponerlo por primera vez al público

--- core/forms.py
+++ core/forms.py
@@ -5,15 +5,15 @@ class HeatSinkForm(forms.Form):
     longitud = forms.FloatField(label="Longitud",help_text='mm')
     direccion_aletas = forms.ChoiceField(
                         label="Direccion de Aletas",
-                        choices=(
-                            ('aletas_apuntan_arriba','arriba'),
-                            ('aletas_apuntan_abajo','abajo')
+                        choices=[
+                            ('aletas_apuntan_arriba','arriba')
+                            #('aletas_apuntan_abajo','abajo')
                             #('aletas_perpendicular_a_suelo','perpendicular')
-                            )
+                            ]
                         )
     centro_x_fuente = forms.FloatField(label="Centro X Fuente",help_text='mm')
-    centro_z_fuente = forms.FloatField(label="Centro Z Fuente",help_text='mm')
+    centro_z_fuente = forms.FloatField(label="Centro Y Fuente",help_text='mm')
     ancho_x_fuente = forms.FloatField(label="Ancho X Fuente",help_text='mm')
-    profundo_z_fuente = forms.FloatField(label="Largo Z Fuente",help_text='mm')
+    profundo_z_fuente = forms.FloatField(label="Largo Y Fuente",help_text='mm')
     calor_fuente = forms.FloatField(label="Calor Fuente",help_text='W')
     temperatura = forms.FloatField(label='Temperatura Ambiente',help_text='C')
